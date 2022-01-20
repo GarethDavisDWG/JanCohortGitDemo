5fdc8c284ee3ab9c0da0b75ffc00ed843b704408
Gareth Davis <gdavis@softwareinstitute.com>
2022-01-20 13:48:58 +0000

    ` Please enter the commit message for your changes. Lines starting

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String PaintName = "Classy";
+        int PaintCoveragePerCanSqrMeter = 10;
+        int wall1LengthInMm = 2000;
+        int wall1HeightInMm = 2000;
+        int wall1AreaInMeterSqr = (wall1HeightInMm/1000) * (wall1LengthInMm/1000);
+        int tinsNeeded = wall1AreaInMeterSqr / PaintCoveragePerCanSqrMeter;
+        if (wall1AreaInMeterSqr%PaintCoveragePerCanSqrMeter!=0)
+        {
+            tinsNeeded++;
+        }
+        PaintName.length();
+        double e = (double)10/4;
+        double d = Math.ceil(e);
+        System.out.println(d);
+    }
+}
